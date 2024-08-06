0d35f6da878e47e9928b8ef1d1f08c3085a8d6b6
Saúl Menchú <82598318+SaulMen@users.noreply.github.com>
2024-08-05 23:23:15 -0600

    Update agente.js

--- agente.js
+++ agente.js
@@ -14,52 +14,68 @@ function getRandom() {
     return Math.floor(Math.random() * 2);
 }
 
+
 function reflex_agent(location, state) {
     if (state === DIRTY_STATE && getRandom() === 1) return CLEAN_ACTION;
     if (state === CLEAN_STATE && getRandom() === 1) return MESS_ACTION;
-
     if (location === LEFT_POSITION) return RIGHT_ACTION;
     if (location === RIGHT_POSITION) return LEFT_ACTION;
 }
 
 
-const traversedStates = []
+const traversedStates = [];
+
 
 const addState = (newState) => {
     const [robot, aState, BState] = newState;
-    const posibleStates = traversedStates.find(state => {
+    const possibleState = traversedStates.find(state => {
         const [r, a, b] = state;
         return a === aState && b === BState && robot === r;
-    })
-    if (!posibleStates) {
+    });
+    if (!possibleState) {
         traversedStates.push([...newState]);
-        console.log({ traversedStates })
+        console.log({ traversedStates });
+        updateTraversedStatesView();
     }
 }
 
-function test(states) {
 
-    if (traversedStates.length === 8) {
-        return document.getElementById("log").innerHTML += `
-        <br>Todos los estados han sido recorridos<br>
-		<ul>
-        ${
-			[...traversedStates].map(state => `
-				<li>${state[0]}:  ${state[1]} - ${state[2]}</li>
-			`).join("")
-		}
-		</ul>
-        `;
+const updateTraversedStatesView = () => {
+    const traversedStatesContainer = document.getElementById("traversedStates");
+    traversedStatesContainer.innerHTML = `
+        <h3>Resumen de Estados Recorridos</h3>
+        <ol type="1">
+            ${traversedStates.map(state => `
+                <li>${state[0]}:  ${state[1]} - ${state[2]}</li>
+            `).join("")}
+        </ol>
+    `;
+    if (traversedStates.length === 8) {        
+        document.getElementById("traversedStates").innerHTML += `
+        <h3>RSe recorrieron todos los estados (8)</h3>`;
+        updateTraversedStatesView();
+        return;
     }
+}
+
+
+function test(states) {
+    if (traversedStates.length === 8) {
+        document.getElementById("log").innerHTML += `
+        <br>Se recorrieron todos los estados (8)<br>`;
+        updateTraversedStatesView();
+        return;
+    }
+
     addState(states);
 
     const [location, leftState, rightState] = states;
     const state = location === LEFT_POSITION ? leftState : rightState;
-
     const action_result = reflex_agent(location, state);
-
+    
     document.getElementById("log").innerHTML += `
-    <br>State: A: ${leftState} | B: ${rightState} Location: ${location} | Action: ${action_result}<br>`;
+    <br>Ubicación: ${location} -> ||Estado: A: ${leftState} -- B: ${rightState}|| <- Acción: ${action_result}<br>`;
+    
 
     if (action_result === CLEAN_ACTION) {
         if (location === LEFT_POSITION) states[1] = CLEAN_STATE;
@@ -71,10 +87,12 @@ function test(states) {
         else if (location === RIGHT_POSITION) states[2] = DIRTY_STATE;
     }
 
-    else if (action_result === RIGHT_ACTION) states[0] = RIGHT_POSITION;
+    if (action_result === RIGHT_ACTION) states[0] = RIGHT_POSITION;
     else if (action_result === LEFT_ACTION) states[0] = LEFT_POSITION;
+
     setTimeout(function () { test(states); }, 600);
 }
+
 
 const states = [LEFT_POSITION, DIRTY_STATE, DIRTY_STATE];
 test(states);
